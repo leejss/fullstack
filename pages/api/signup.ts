e29d4e0714f6b5355d0a518a4dc689cb04a1b38a
leejss <goldemshine@gmail.com>
2022-03-30 09:40:40 +0900

    clean

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -5,8 +5,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const salt = genSaltSync();
   const {} = req.body;
+  // Generate a user
   try {
-  } catch (
-    
-  ) {}
+  } catch {}
 };
